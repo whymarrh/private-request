7e2dc6375a4e272959c27d641658093f4d837dfe
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-07-23 20:22:03 -0230

    Update baretest types

--- types/baretest.d.ts
+++ types/baretest.d.ts
@@ -3,9 +3,9 @@ declare module 'baretest' {
     (name: string, fn: () => Promise<void>): void;
     run(): Promise<boolean>;
     skip(fn: () => Promise<void>): void;
-    // This isn't real, but the fn does ignore its args
+    // This isn't real, but skip ignores its args
     skip(name: string, fn: () => Promise<void>): void;
-    // This isn't real, but the fn does ignore its args
+    // This isn't real, but skip ignores its args
     skip(name: string): void;
     before(fn: () => Promise<void>): void;
     after(fn: () => Promise<void>): void;
