3e3598131ff2175e7535325892add9b7ccfda22e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-07-23 20:54:48 -0230

    Remove unused pure-random-number types

--- types/pure-random-number.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'pure-random-number' {
-  export function randomSync(minimum: number, maximum: number): number;
-
-  export default function secureRandomNumber(minimum: number, maximum: number): Promise<number>;
-}
